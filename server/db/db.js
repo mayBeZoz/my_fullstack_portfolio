6e38d920e6a167d0c2a88305d5303ec5e0f3e3cc
zeyad tamer <zeyadtamer101@gmail.com>
2024-06-06 04:19:11 +0300

    trying to fix server deployment issue

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const mongoose = require( "mongoose")
 async function databaseConnection () {
      
     const URI = process.env.MONGO_URI
-    mongoose.connect(URI)
+    mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> {
         console.log("connected to the database successfully")
     })
